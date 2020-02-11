0a6fb891e24cea78874874b552cf25498d9201eb
Amruth sagar <buggiyoo555@gmail.com>
2020-02-11 22:10:04 +0530

    Calculator

---
+++ calculator/src/main/java/com/epam/calculator/Calculator.java
@@ -0,0 +1,26 @@
+package com.epam.calculator;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class Calculator
+{
+	public int addition(int a,int b)
+	{
+		return a+b;
+	}
+	public  int subtraction(int a,int b)
+	{
+		return a-b;
+	}
+	public int multiplication(int a,int b)
+	{
+		return a*b;
+	}
+	public int division(int a,int b)
+	{
+		return a/b;
+	}
+}
